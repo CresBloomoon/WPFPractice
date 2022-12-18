cde2a229fb001e0a6c3f9cba9bac49ea2e72ca87
CresBloomoon <metroidm2421@gmail.com>
2022-12-18 13:53:24 +0900

    ViewModelのプロパティと、MainViewのテキストボックス間をバインディングさせました。

--- DivisionPractice/DivisionPractice/ViewModels/MainViewModel.cs
+++ DivisionPractice/DivisionPractice/ViewModels/MainViewModel.cs
@@ -8,6 +8,36 @@ namespace DivisionPractice.ViewModels
 {
     internal class MainViewModel : ViewModelBase
     {
+        private string _lhs;
+        /// <summary>
+        /// 割られる数に指定される文字列を取得または設定します。
+        /// </summary>
+        public string Lhs
+        {
+            get { return this._lhs; }
+            set { SetProperty(ref this._lhs, value); }
+        }
+
+        private string _rhs;
+        /// <summary>
+        /// 割る数に指定される文字列を取得または設定します。
+        /// </summary>
+        public string Rhs
+        {
+            get { return this._rhs; }
+            set { SetProperty(ref this._rhs, value); }
+        }
+
+        private string _result;
+        /// <summary>
+        /// 計算結果を文字列として取得します。
+        /// </summary>
+        public string Result
+        {
+            get { return this._result; }
+            set { SetProperty(ref this._result, value); }
+        }
+
 
     }
 }
