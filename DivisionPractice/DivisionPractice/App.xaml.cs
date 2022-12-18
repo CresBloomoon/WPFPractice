67b7957cc33f3d23d50cf53557258a0f79bbc326
CresBloomoon <metroidm2421@gmail.com>
2022-12-18 13:37:20 +0900

    MainVIewを実装しました

---
+++ DivisionPractice/DivisionPractice/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DivisionPractice.ViewModels;
+using DivisionPractice.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DivisionPractice
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var w = new MainView();
+            var vm = new MainViewModel();
+            w.DataContext = vm;
+            w.Show();
+        }
+    }
+}
---
+++ DivisionPractice/DivisionPractice/ViewModels/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace DivisionPractice.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// PropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T target, T value,
+            [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(target, value)) return false;
+            target = value;
+            var h = this.PropertyChanged;
+            if (h != null) h(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
